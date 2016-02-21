4509c161c4bd69a1cb196f660fae8442fe187f54
tranbenny <tran.benny123@gmail.com>
2016-02-20 17:05:24 -0800

    created simple thread in activity example

---
+++ android/threads.java
@@ -0,0 +1,40 @@
+// example background thread in an an activity 
+// threads not recommened for long-term acesses
+
+
+// Activity.runOnUiThread method
+// View.post
+
+public class MyActivity extends Activity {
+
+	Thread _workerThread;
+	TextView _defaultTextView;
+	String _defaultTextViewTempString;
+
+
+	public void doWork() {
+		_workerThread = new Thread(new Runnable() {
+			// work that runs in a background thread
+			public void run() {
+				// code to run in background thread
+				// updates to display current status of tasks and regurlarly updates
+				updateDisplay("Fetching some info");
+				updateDisplay("Fetching user info");
+				updateDisplay("Done"); 
+			}
+		});
+		_workerThread.start(); // starts run method of thread
+	}
+
+
+	// updates text view with string accessed from another background thread
+	private void updateDisplay(String message) {
+		_defaultTextViewTempString = message;
+		_defaultTextView.post(new Runnable() {
+			public void run() {
+				_defaultTextView.setText(_defaultViewTempString);
+			}
+		});
+	}
+
+}
